e1f5e0416375da7c10e989bf42a7f7e492a2e1a8
Socks <patricksutcliffe123@gmail.com>
2022-06-09 15:09:27 -0400

    fixing basic structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import './App.css';
+import React, {useState} from 'react';
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+import Page from './components/Page';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <NavBar></NavBar>
+      </Header>
+      <main>
+        <Page></Page>
+      </main>
+      <Footer />
     </div>
   );
 }
