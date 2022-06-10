21060ad0b1a94a478c946894ce4b9cc39a9ae74f
Socks <patricksutcliffe123@gmail.com>
2022-06-10 13:05:01 -0400

    finsihed functionality

--- src/components/PageContent/index.js
+++ src/components/PageContent/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-function PageContent(){
-
+function PageContent(props){
+    return(
+        <div>
+            {props.children}
+        </div>
+    )
 }
 
 export default PageContent
--- src/components/Projs/index.js
+++ src/components/Projs/index.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { removeHyphensAndCapitalize } from '../../utils/helpers';
 
-function Projs(){
-
+function Projs({project}){
+    const { name, repo, link, description } = project;
+    return(
+    <div className="project" key={name}>
+      <img
+        src={require(`../../assets/images/${name}.jpg`)}
+        alt={removeHyphensAndCapitalize(name)}
+        className="project-bg"
+      />
+      <div className="project-text">
+        <h3>
+          <a href={link}>{removeHyphensAndCapitalize(name)}</a>{' '}
+          <a href={repo}>
+            <i className="fab fa-github"></i>
+          </a>
+        </h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Projs
